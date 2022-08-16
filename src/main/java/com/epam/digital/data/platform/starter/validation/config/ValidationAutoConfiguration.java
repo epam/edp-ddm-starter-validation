7eba0a375678937257f04cb7d58060a7895a8f52
Vladyslav Chuhai <vladyslav_chuhai@epam.com>
2022-08-16 13:58:22 +0300

    [EDP-DDM-16617] usage of ddm-form-validation-client
    Change-Id: I6582f6e1f1630f8528d5af7f35b60676bcf0f9eb

--- src/main/java/com/epam/digital/data/platform/starter/validation/config/ValidationAutoConfiguration.java
+++ src/main/java/com/epam/digital/data/platform/starter/validation/config/ValidationAutoConfiguration.java
@@ -16,15 +16,15 @@
 
 package com.epam.digital.data.platform.starter.validation.config;
 
-import com.epam.digital.data.platform.integration.formprovider.client.FormManagementProviderClient;
+import com.epam.digital.data.platform.integration.formprovider.client.FormValidationClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(value = "form-management-provider.url")
+@ConditionalOnProperty(value = "form-submission-validation.url")
 @ComponentScan(basePackages = "com.epam.digital.data.platform.starter.validation")
-@EnableFeignClients(clients = FormManagementProviderClient.class)
+@EnableFeignClients(clients = FormValidationClient.class)
 public class ValidationAutoConfiguration {
 }
